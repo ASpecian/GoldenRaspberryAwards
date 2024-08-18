28c095205988ae2d5560ad2f7b7ddefc3aaae24c
ASpecian <andrespecian@gmail.com>
2024-08-17 22:15:38 -0300

    Primeiro commit

---
+++ gra/src/test/java/com/asc/ts/gra/GoldenRaspberryAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asc.ts.gra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
